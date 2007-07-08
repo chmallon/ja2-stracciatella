8c617839b1b27d047c083ae9a89b4de0c768d0f4
Christoph Mallon <christoph.mallon@gmx.de>
2007-07-08 00:29:55 +0000

    Switch off debug messages.

--- sgp/FileMan.c
+++ sgp/FileMan.c
@@ -860,7 +860,6 @@ BOOLEAN MakeFileManDirectory(const char *pcDirectory)
 BOOLEAN RemoveFileManDirectory(const char *pcDirectory, BOOLEAN fRecursive)
 {
 #if 1 // XXX TODO
-	fprintf(stderr, "===> %s:%d: IGNORING %s(\"%s\", %s)\n", __FILE__, __LINE__, __func__, pcDirectory, fRecursive ? "TRUE" : "FALSE");
 	return FALSE;
 	UNIMPLEMENTED();
 #else
@@ -1220,7 +1219,6 @@ UINT32 FileGetAttributes(const char *strFilename)
 BOOLEAN FileClearAttributes(const char *strFilename)
 {
 #if 1 // XXX TODO
-	fprintf(stderr, "===> %s:%d: IGNORING %s(\"%s\")\n", __FILE__, __LINE__, __func__, strFilename);
 	return FALSE;
 	UNIMPLEMENTED();
 #else
