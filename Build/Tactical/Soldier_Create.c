8ffb529ce4b78ad215e26df05d2724af91192cbc
Christoph Mallon <christoph.mallon@gmx.de>
2007-11-19 08:14:03 +0000

    Remove redundant assignment.

--- Build/Tactical/Soldier_Create.c
+++ Build/Tactical/Soldier_Create.c
@@ -1092,7 +1092,6 @@ static void InitSoldierStruct(SOLDIERTYPE* pSoldier)
 	pSoldier->uiAIDelay							= 100;
 
 	pSoldier->iLight								= -1;
-	pSoldier->iFaceIndex						= -1;
 
 	// Set update time to new speed
 	pSoldier->ubDesiredHeight				= NO_DESIRED_HEIGHT;
