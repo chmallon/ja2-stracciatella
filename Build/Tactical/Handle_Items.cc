d438ba2b60b137c1a7311e69b61bef5e51006cad
Christoph Mallon <christoph.mallon@gmx.de>
2009-01-10 14:49:34 +0000

    Resolve potential use of uninitialised variable.

--- Build/Tactical/Handle_Items.cc
+++ Build/Tactical/Handle_Items.cc
@@ -2678,6 +2678,10 @@ static void StartBombMessageBox(SOLDIERTYPE* const s, INT16 const gridno)
 	{
 		text = TacticalStr[CHOOSE_REMOTE_FREQUENCY_STR];
 	}
+	else
+	{
+		return;
+	}
 	DoMessageBox(MSG_BOX_BASIC_SMALL_BUTTONS, text, GAME_SCREEN, MSG_BOX_FLAG_FOUR_NUMBERED_BUTTONS, BombMessageBoxCallBack, NULL);
 }
 
